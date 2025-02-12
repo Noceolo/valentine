23723f72fd45289a3f23b78a6683f58125baf019
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-02-12 19:05:26 +0100

    Added confetti script on yesBtn

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+
+
 const container = document.getElementById('container');
 const noBtn = document.getElementById('noBtn');
+const yesBtn = document.getElementById('yesBtn');
+
+const jsConfetti = new JSConfetti()
+
 
 function randomChange() {
     const 
-        { width: containerWidth, height: containerHeight } = container.getBoundingClientRect(),
-        { width: noBtnWidth, height: noBtnHeight } = noBtn.getBoundingClientRect(),
-        i = Math.floor(Math.random() * (containerWidth - noBtnWidth)) + 1,
-        j = Math.floor(Math.random() * (containerHeight - noBtnHeight)) + 1;
+    { width: containerWidth, height: containerHeight } = container.getBoundingClientRect(),
+    { width: noBtnWidth, height: noBtnHeight } = noBtn.getBoundingClientRect(),
+    i = Math.floor(Math.random() * (containerWidth - noBtnWidth)) + 1,
+    j = Math.floor(Math.random() * (containerHeight - noBtnHeight)) + 1;
     noBtn.style.left = i + "px";
     noBtn.style.top = j + "px";
 }
 
+function congrats() {
+    jsConfetti.addConfetti({
+        emojis: ['❤️','💝','🩷','❤']
+    })
+}
+
 noBtn.addEventListener("click", randomChange);
+yesBtn.addEventListener("click", congrats);
