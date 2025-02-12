46b10a3e8958a412eba0cb2ade66b9f500199793
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-02-12 18:03:52 +0100

    Added the random movement fct for the no button

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('container');
+const noBtn = document.getElementById('noBtn');
+
+function randomChange() {
+    const 
+        { width: containerWidth, height: containerHeight } = container.getBoundingClientRect(),
+        { width: noBtnWidth, height: noBtnHeight } = noBtn.getBoundingClientRect(),
+        i = Math.floor(Math.random() * (containerWidth - noBtnWidth)) + 1,
+        j = Math.floor(Math.random() * (containerHeight - noBtnHeight)) + 1;
+    noBtn.style.left = i + "px";
+    noBtn.style.top = j + "px";
+}
+
+noBtn.addEventListener("click", randomChange);
